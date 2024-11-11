ca190a4f374f3ed69aab1b90f358eddc1550caf1
jordan <jordan@bitola.com>
2024-11-11 14:52:12 +0700

    Rootstock RPC personal API key

--- deployRootstock.sh
+++ deployRootstock.sh
@@ -6,5 +6,6 @@
 source .env
 
 # To deploy and verify our contract
-# forge script script/Multiprotocol.s.sol:Deploy --slow --legacy --rpc-url "https://public-node.testnet.rsk.co" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
-forge script script/Multiprotocol.s.sol:Deploy --legacy --rpc-url "https://rootstock-testnet.g.alchemy.com/v2/IhgQBvRgybGlyu1Jo8-f9PpIphc4iqw3" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
+#forge script script/Multiprotocol.s.sol:Deploy --legacy --rpc-url "https://public-node.testnet.rsk.co" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
+forge script script/Multiprotocol.s.sol:Deploy --legacy --rpc-url "https://rpc.testnet.rootstock.io/lZZVyJCscc9luAu3ByNcsjI9UOAb9H-T" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
+#forge script script/Multiprotocol.s.sol:Deploy --legacy --rpc-url "https://rootstock-testnet.g.alchemy.com/v2/IhgQBvRgybGlyu1Jo8-f9PpIphc4iqw3" --sender $SENDER --private-key $PRIVATE_KEY --broadcast -vvvv
